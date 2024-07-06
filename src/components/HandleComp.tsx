5755be6f2fe8cc194efdfe7e040e5c85ee88ea27
Hardik Arora <hardikarora826@gmail.com>
2024-07-07 02:30:10 +0530

    design page 2

---
+++ src/components/HandleComp.tsx
@@ -0,0 +1,7 @@
+const HandleComp = () => {
+  return (
+    <div className="size-5 rounded-full shadow bg-white border-zinc-500 transition hover:bg-primary" />
+  );
+};
+
+export default HandleComp;
