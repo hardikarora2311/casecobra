31c39424e3cecb93acfa902a1c70274ba0189b2e
Hardik Arora <hardikarora826@gmail.com>
2024-07-09 20:41:50 +0530

    metadata check- all cmplt

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,3 +13,33 @@ export const formatPrice = (price: number) => {
   });
   return formatter.format(price);
 };
+
+export function constructMetaData({
+  title = "CaseCobra - Custom High-quality phone cases.",
+  description = "Create custom high-quality phone cases in seconds. Upload your image, choose your case type, and get your case delivered in 3-5 days.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@HardikA2311_",
+    },
+    icons,
+  };
+}
